7ea483f578f4b112683e877537d9417cb1bfa322
Morgan Harmon <morganharmon@gmail.com>
2022-04-28 11:27:38 -0700

    Add event listeners for dropdowns

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 // import functions and grab DOM elements
-const 
+const climatePic = document.getElementById('climatePic');
+const archiPic = document.getElementById('archiPic');
+const castlePic = document.getElementById('castlePic');
+const climateSelect = document.getElementById('climateSelect');
+const archiSelect = document.getElementById('archiSelect');
+const castleSelect = document.getElementById('castleSelect');
+
 // let state
 
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+climateSelect.addEventListener('change', (e) => {
+    const climate = e.target.value;
+    climatePic.src = `./assets/${climate}.jpeg`;
+});
+archiSelect.addEventListener('change', (e) => {
+    const archi = e.target.value;
+    archiPic.src = `./assets/${archi}.jpeg`;
+});
+castleSelect.addEventListener('change', (e) => {
+    const castle = e.target.value;
+    castlePic.src = `./assets/${castle}.jpeg`;
+});
